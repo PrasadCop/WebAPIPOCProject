7810c63c48716f6a31c5b28f79aea4b88aacd253
PrasadCop <146869614+PrasadCop@users.noreply.github.com>
2023-10-05 14:34:09 +0200

    Service change

--- Object Repository/SOAPUI/SOAP Service/CountryInfoServiceSoapBinding/ListOfCurrenciesByCode.rs
+++ Object Repository/SOAPUI/SOAP Service/CountryInfoServiceSoapBinding/ListOfCurrenciesByCode.rs
@@ -28,7 +28,7 @@
    <soapBody>&lt;soapenv:Envelope xmlns:soapenv=&quot;http://schemas.xmlsoap.org/soap/envelope/&quot; xmlns:web=&quot;http://www.oorsprong.org/websamples.countryinfo&quot;>
    &lt;soapenv:Header/>
    &lt;soapenv:Body>
-      &lt;web:ListOfCurrenciesByCode/>
+      &lt;web:ListOfCurrenciesByCode/>US&lt;web:ListOfCurrenciesByCode/>
    &lt;/soapenv:Body>
 &lt;/soapenv:Envelope></soapBody>
    <soapHeader></soapHeader>
@@ -37,5 +37,18 @@
    <soapServiceFunction>ListOfCurrenciesByCode</soapServiceFunction>
    <socketTimeout>-1</socketTimeout>
    <useServiceInfoFromWsdl>false</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
    <wsdlAddress>http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL</wsdlAddress>
 </WebServiceRequestEntity>
